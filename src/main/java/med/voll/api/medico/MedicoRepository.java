d0c55545e6671464d326d283ef14e2f4228343e1
Sávio Martins <saviossmg@gmail.com>
2023-08-02 10:36:53 -0300

    Parte 1
    Primeira parte da API VollMed

---
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+
+    //findAllBy nome do atributo e valor esperado
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+}
